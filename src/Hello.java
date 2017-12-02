353a2ed5c10daf1aa19197fda42f2e53d789cb4d
teng654321 <1191391943@qq.com>
2017-12-02 16:32:49 +0800

    IDEA 整合github

--- src/Hello.java
+++ src/Hello.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class Hello {
     public static void main(String[] args){
-        //System.out.println("hello world");
+        System.out.println("hello world");
         Random random=new Random();
         int n=random.nextInt(10);
         System.out.print(n);
